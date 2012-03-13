d0d1e7d4738ec32b34aa74cccd2104e6c86bb9c9
Chris James <qui666@gmail.com>
2012-03-13 21:48:54 +0000

    Chris - fixed the json thing

--- cs/site.coffee
+++ cs/site.coffee
@@ -3,13 +3,9 @@ class SpringerLite
 	constructor: ->
 		$("#search-form").submit ->
 			url = "http://api.springer.com/metadata/json?q=physics&api_key=ueukuwx5guegu4ahjc6ajq8w"
-			$.ajax url,
-			    type: 'GET'
-			    dataType: 'json'
-			    error: (jqXHR, textStatus, errorThrown) ->
-			        $('body').append "AJAX Error: #{textStatus}"
-			    success: (data, textStatus, jqXHR) ->
-			        $('body').append "Successful AJAX call: #{data}"
-						false
+			$.getJSON(url, (data) ->
+				console.log data
+			)
+			false
 $ ->
 	site = new SpringerLite()
--- js/site.js
+++ js/site.js
@@ -7,15 +7,8 @@
       $("#search-form").submit(function() {
         var url;
         url = "http://api.springer.com/metadata/json?q=physics&api_key=ueukuwx5guegu4ahjc6ajq8w";
-        $.ajax(url, {
-          type: 'GET',
-          dataType: 'json',
-          error: function(jqXHR, textStatus, errorThrown) {
-            return $('body').append("AJAX Error: " + textStatus);
-          },
-          success: function(data, textStatus, jqXHR) {
-            return $('body').append("Successful AJAX call: " + data);
-          }
+        $.getJSON(url, function(data) {
+          return console.log(data);
         });
         return false;
       });
