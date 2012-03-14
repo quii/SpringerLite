1f5c73dab0fa5f811ddb8181f9bf0dea9c74af27
Chris James <qui666@gmail.com>
2012-03-14 18:53:32 +0000

    Chris - Typo and made the button give some feedback whilst searching

--- cs/site.coffee
+++ cs/site.coffee
@@ -3,6 +3,7 @@ class SpringerLite
 	constructor: ->
 		$("#search-form").submit (e) ->
 			e.preventDefault()
+			$("#search-button").attr("value", "Searching...")
 			term = $("#search").val()
 			url = "http://api.springer.com/metadata/jsonp?q=#{term}&api_key=ueukuwx5guegu4ahjc6ajq8w&callback=?"
 			$.ajax 
@@ -11,6 +12,7 @@ class SpringerLite
 				type: 'GET'
 				success: (json) -> 
 					console.log(json)
+					$("#search-button").attr("value", "Search")
 					$("#results").html(Mustache.to_html($('#template').html(), json));
 
 $ ->
--- js/site.js
+++ js/site.js
@@ -7,15 +7,16 @@
       $("#search-form").submit(function(e) {
         var term, url;
         e.preventDefault();
+        $("#search-button").attr("value", "Searching...");
         term = $("#search").val();
         url = "http://api.springer.com/metadata/jsonp?q=" + term + "&api_key=ueukuwx5guegu4ahjc6ajq8w&callback=?";
-        console.log(url);
         return $.ajax({
           url: url,
           dataType: 'jsonp',
           type: 'GET',
           success: function(json) {
             console.log(json);
+            $("#search-button").attr("value", "Search");
             return $("#results").html(Mustache.to_html($('#template').html(), json));
           }
         });
