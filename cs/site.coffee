d9c2988e6b0721a39db84c95cd3eaa8a1606981c
Chris James <qui666@gmail.com>
2012-03-15 00:27:11 +0000

    Updated css and refactored cs a bit

--- cs/site.coffee
+++ cs/site.coffee
@@ -1,19 +1,22 @@
 class SpringerLite
 
 	constructor: ->
-		$("#search-form").submit (e) ->
-			e.preventDefault()
-			$("#search-button").attr("value", "Searching...")
-			term = $("#search").val()
-			url = "http://api.springer.com/metadata/jsonp?q=#{term}&api_key=ueukuwx5guegu4ahjc6ajq8w&callback=?"
-			$.ajax 
-				url: url
-				dataType: 'jsonp'
-				type: 'GET'
-				success: (json) -> 
-					console.log(json)
-					$("#search-button").attr("value", "Search")
-					$("#results").html(Mustache.to_html($('#template').html(), json));
+		$("#search-form").submit (e) =>
+			e.preventDefault() 
+			this.doSearch(1, e)
+
+	doSearch: (page) ->
+		$("#search-button").attr("value", "Searching...")
+		term = $("#search").val()
+		url = "http://api.springer.com/metadata/jsonp?q=#{term}&api_key=ueukuwx5guegu4ahjc6ajq8w&callback=?"
+		$.ajax 
+			url: url
+			dataType: 'jsonp'
+			type: 'GET'
+			success: (json) -> 
+				console.log(json)
+				$("#search-button").attr("value", "Search")
+				$("#results").html(Mustache.to_html($('#template').html(), json));
 
 $ ->
 	site = new SpringerLite()
--- js/site.js
+++ js/site.js
@@ -4,24 +4,29 @@
   SpringerLite = (function() {
 
     function SpringerLite() {
+      var _this = this;
       $("#search-form").submit(function(e) {
-        var term, url;
         e.preventDefault();
-        $("#search-button").attr("value", "Searching...");
-        term = $("#search").val();
-        url = "http://api.springer.com/metadata/jsonp?q=" + term + "&api_key=ueukuwx5guegu4ahjc6ajq8w&callback=?";
-        return $.ajax({
-          url: url,
-          dataType: 'jsonp',
-          type: 'GET',
-          success: function(json) {
-            console.log(json);
-            $("#search-button").attr("value", "Search");
-            return $("#results").html(Mustache.to_html($('#template').html(), json));
-          }
-        });
+        return _this.doSearch(1, e);
       });
     }
+
+    SpringerLite.prototype.doSearch = function(page) {
+      var term, url;
+      $("#search-button").attr("value", "Searching...");
+      term = $("#search").val();
+      url = "http://api.springer.com/metadata/jsonp?q=" + term + "&api_key=ueukuwx5guegu4ahjc6ajq8w&callback=?";
+      return $.ajax({
+        url: url,
+        dataType: 'jsonp',
+        type: 'GET',
+        success: function(json) {
+          console.log(json);
+          $("#search-button").attr("value", "Search");
+          return $("#results").html(Mustache.to_html($('#template').html(), json));
+        }
+      });
+    };
 
     return SpringerLite;
 
