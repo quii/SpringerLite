9c62ff4f6af932bd4b58dfecd98b7a80a78aa4e0
Chris James <qui666@gmail.com>
2012-03-17 12:17:28 +0000

    Basic in memory caching done

--- cs/site.coffee
+++ cs/site.coffee
@@ -1,35 +1,46 @@
 class SearchResult
 	constructor: (@terms, @html) ->
 
-	@getHtml: (results, selectedTerm) -> (r for r in results when r.terms == selectedTerm)[0].html
+class SearchResultCache
+	results: []
+
+	addResultToCache: (term, renderedHTML) -> @results.push(new SearchResult(term, renderedHTML))
+
+	getHtml: (term) -> @findResult(term)[0].html
+
+	exists: (term) -> @findResult(term).length>0
+
+	findResult: (term) -> (r for r in @results when r.terms == term)
 
 class SpringerLite
 
+	resultsCache: {}
+
 	constructor: ->
+		@resultsCache = new SearchResultCache
 		$("#search-form").submit (e) =>
 			e.preventDefault() 
 			this.doSearch(1)
 
-	results: []
-
 	doSearch: (page) ->
 		$("#search-button").attr("value", "Searching...")
 		term = $("#search").val()
-		url = "http://api.springer.com/metadata/jsonp?q=#{term}&api_key=ueukuwx5guegu4ahjc6ajq8w&callback=?"
-		$.ajax 
-			url: url
-			dataType: 'jsonp'
-			type: 'GET'
-			success: (json) => 
-				$("#search-button").attr("value", "Search")
-				renderedHTML = Mustache.to_html($('#template').html(), json)
-				@addResultToCache(term, renderedHTML)
-				@renderResult(term)
 
-	addResultToCache: (term, renderedHTML) ->
-		@results.push(new SearchResult(term, renderedHTML))
+		if(@resultsCache.exists(term))
+			@renderResult(term)
+		else
+			url = "http://api.springer.com/metadata/jsonp?q=#{term}&api_key=ueukuwx5guegu4ahjc6ajq8w&callback=?"
+			$.ajax 
+				url: url
+				dataType: 'jsonp'
+				type: 'GET'
+				success: (json) => 
+					$("#search-button").attr("value", "Search")
+					renderedHTML = Mustache.to_html($('#template').html(), json)
+					@resultsCache.addResultToCache(term, renderedHTML)
+					@renderResult(term)
 
-	renderResult: (term) -> $("#results").html(SearchResult.getHtml(@results, term))
+	renderResult: (term) -> $("#results").html(@resultsCache.getHtml(term))
 
 $ ->
 	site = new SpringerLite()
--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
 (function() {
-  var SearchResult, SpringerLite;
+  var SearchResult, SearchResultCache, SpringerLite;
 
   SearchResult = (function() {
 
@@ -8,61 +8,82 @@
       this.html = html;
     }
 
-    SearchResult.getHtml = function(results, selectedTerm) {
-      var r;
-      return ((function() {
-        var _i, _len, _results;
-        _results = [];
-        for (_i = 0, _len = results.length; _i < _len; _i++) {
-          r = results[_i];
-          if (r.terms === selectedTerm) _results.push(r);
-        }
-        return _results;
-      })())[0].html;
+    return SearchResult;
+
+  })();
+
+  SearchResultCache = (function() {
+
+    function SearchResultCache() {}
+
+    SearchResultCache.prototype.results = [];
+
+    SearchResultCache.prototype.addResultToCache = function(term, renderedHTML) {
+      return this.results.push(new SearchResult(term, renderedHTML));
     };
 
-    return SearchResult;
+    SearchResultCache.prototype.getHtml = function(term) {
+      return this.findResult(term)[0].html;
+    };
+
+    SearchResultCache.prototype.exists = function(term) {
+      return this.findResult(term).length > 0;
+    };
+
+    SearchResultCache.prototype.findResult = function(term) {
+      var r, _i, _len, _ref, _results;
+      _ref = this.results;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        r = _ref[_i];
+        if (r.terms === term) _results.push(r);
+      }
+      return _results;
+    };
+
+    return SearchResultCache;
 
   })();
 
   SpringerLite = (function() {
 
+    SpringerLite.prototype.resultsCache = {};
+
     function SpringerLite() {
       var _this = this;
+      this.resultsCache = new SearchResultCache;
       $("#search-form").submit(function(e) {
         e.preventDefault();
         return _this.doSearch(1);
       });
     }
 
-    SpringerLite.prototype.results = [];
-
     SpringerLite.prototype.doSearch = function(page) {
       var term, url,
         _this = this;
       $("#search-button").attr("value", "Searching...");
       term = $("#search").val();
-      url = "http://api.springer.com/metadata/jsonp?q=" + term + "&api_key=ueukuwx5guegu4ahjc6ajq8w&callback=?";
-      return $.ajax({
-        url: url,
-        dataType: 'jsonp',
-        type: 'GET',
-        success: function(json) {
-          var renderedHTML;
-          $("#search-button").attr("value", "Search");
-          renderedHTML = Mustache.to_html($('#template').html(), json);
-          _this.addResultToCache(term, renderedHTML);
-          return _this.renderResult(term);
-        }
-      });
-    };
-
-    SpringerLite.prototype.addResultToCache = function(term, renderedHTML) {
-      return this.results.push(new SearchResult(term, renderedHTML));
+      if (this.resultsCache.exists(term)) {
+        return this.renderResult(term);
+      } else {
+        url = "http://api.springer.com/metadata/jsonp?q=" + term + "&api_key=ueukuwx5guegu4ahjc6ajq8w&callback=?";
+        return $.ajax({
+          url: url,
+          dataType: 'jsonp',
+          type: 'GET',
+          success: function(json) {
+            var renderedHTML;
+            $("#search-button").attr("value", "Search");
+            renderedHTML = Mustache.to_html($('#template').html(), json);
+            _this.resultsCache.addResultToCache(term, renderedHTML);
+            return _this.renderResult(term);
+          }
+        });
+      }
     };
 
     SpringerLite.prototype.renderResult = function(term) {
-      return $("#results").html(SearchResult.getHtml(this.results, term));
+      return $("#results").html(this.resultsCache.getHtml(term));
     };
 
     return SpringerLite;
